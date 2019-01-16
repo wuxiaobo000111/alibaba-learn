caf6d0713ad9b6c60c293a050cd771874e5529e4
wuxiaobo <wuxiaobo@didachuxing.com>
2019-01-16 19:54:59 +0800

    nacos作为spring boot的配置中心

---
+++ nacos-spring-cloud/nacos-spring-cloud-config/src/main/java/com/bobo/nacosspringcloudconfig/NacosSpringCloudConfigApplication.java
@@ -0,0 +1,13 @@
+package com.bobo.nacosspringcloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosSpringCloudConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosSpringCloudConfigApplication.class, args);
+    }
+}
+
---
+++ nacos-spring-cloud/nacos-spring-cloud-config/src/main/java/com/bobo/nacosspringcloudconfig/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.bobo.nacosspringcloudconfig.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wuxiaobo@didachuxing.com
+ * @create 2019-01-16 19:42
+ **/
+@RestController
+@RefreshScope
+public class HelloController {
+    @Value("${name:default}")
+    private String name;
+
+    @RequestMapping("/get")
+    public String get() {
+        return name;
+    }
+}
